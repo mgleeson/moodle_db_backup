9727dd5b715d8f4e0ac9ff523242333578926c12
Matt Gleeson <mgleeson@users.noreply.github.com>
2018-02-15 08:43:02 +1000

    using pv to monitor progress instead of having verbose tar output

--- moodlefilesbackup.sh
+++ moodlefilesbackup.sh
@@ -2,30 +2,37 @@
 ######
 # moodlefilesbackup.sh
 # @author: Matt Gleeson <matt@mattgleeson.net>
-# @version: 1.00
+# @version: 1.01
 # @license: GPL2
 ######
-versionno="version: 1.00"
+versionno="version: 1.02"
 
 # set -u
+
+. /root/scripts/checkerr.inc.sh
+
 
 ############
 ## Usage
 usage="\
-Usage: 	moodlefilesbackup [-h] [--help] [-m][--moodlepath=PATH] [-b][--backuppath=PATH]
-		[-e][--email=EMAILADDRESS] [--version]"
+Usage:  moodlefilesbackup [-h] [--help] [-m][--moodlepath=PATH] [-b][--backuppath=PATH]
+                [-e][--email=EMAILADDRESS] [--version]"
 
-		
+
 ##### for when stuff go wrong
 function err_exit
 {
-	echo ""
-	echo "$@" 1>&2
-	echo "error at line: ${LINENO}"
-	exit 1
-}		
+        echo ""
+        echo "$@" 1>&2
+        echo "error at line: ${LINENO}"
+        exit 1
+}
 
 ##### ROOT CHECK
+
+which pv
+checkerr "$_" "$?"
+
 ## must run as root
 Check_if_root ()
 {
@@ -41,7 +48,7 @@ Check_if_root
 #### Check for previous moodle config vars file
 if [ -e ~/mdl_conf.sh ]; then
         rm -f ~/mdl_conf.sh || err_exit
-		echo "existing mdl_conf removed [OK]"
+                echo "existing mdl_conf removed [OK]"
 fi
 
 ################################
@@ -51,23 +58,23 @@ do
 case $PARAMS in
     -m=*|--moodlepath=*)
     MOODLEPATH="${PARAMS#*=}"
-	MOODLEPATH=${MOODLEPATH%/}
-    shift 
+        MOODLEPATH=${MOODLEPATH%/}
+    shift
     ;;
     -b=*|--backuppath=*)
     BACKUPPATH="${PARAMS#*=}"
-	BACKUPPATH=${BACKUPPATH%/}
-    shift 
+        BACKUPPATH=${BACKUPPATH%/}
+    shift
     ;;
-	-d|--debug)
+        -d|--debug)
     DEBUG="TRUE"
-	shift 
+        shift
     ;;
     --version|-v*)
          echo ${versionno}; shift ;;
       -- )     # Stop option processing
         shift; break ;;
-      - )	# Use stdin as input.
+      - )       # Use stdin as input.
         break ;;
       -* )
         echo "${usage}" 1>&2; exit 1 ;;
@@ -79,13 +86,15 @@ done
 
 
 if [ ! -n "${MOODLEPATH}" ]; then
-			 echo "no moodle path specified?"
-			 err_exit;
+                         echo "no moodle path specified?"
+                         err_exit;
 fi
 if [ ! -n "${BACKUPPATH}" ]; then
-			 echo "no backup path specified?"
-			 err_exit;
+                         echo "no backup path specified?"
+                         err_exit;
 fi
+
+
 ##### END PARAMETERS/ARGUMENTS PARSER
 
 #############
@@ -96,61 +105,65 @@ grep -P -o '(?<=^\$CFG->)(\w*)\s*=\s?(?:\x27)(.*)(?:\x27)(?=\;)' ${MOODLEPATH}/c
 
 #############
 ## vars
-_sitename=`echo "SELECT shortname FROM ${prefix}course WHERE sortorder = 1" | mysql ${dbname} -u ${dbuser} -p${dbpass} -s -N | tr '/' '-' | tr ' ' '_'`
+_sitename=`echo "SELECT shortname FROM ${prefix}course WHERE sortorder = 1" | mysql -h ${dbhost} ${dbname} -u ${dbuser} -p${dbpass} -s -N | tr '/' '-' | tr ' ' '_'`
 _now=$(date +%Y-%m-%d--%H%M%S)
 _files_zipfile="${BACKUPPATH}/${_sitename}_files_backup_${_now}.tar.gz"
-_data_zipfile="${BACKUPPATH}/${_sitename}_data_backup_${_now}.tar.gz"
+_data_zipfile="${BACKUPPATH}/${_sitename}_moodledata_backup_${_now}.tar.gz"
 ## moodles such as bitnami stack moodles specify wwwroot programatically so this fails on them
-# sitename="`echo ${wwwroot} | sed -e 's/^http:\/\///g' -e 's/^https:\/\///g' | tr '/' '-'`"   
+# sitename="`echo ${wwwroot} | sed -e 's/^http:\/\///g' -e 's/^https:\/\///g' | tr '/' '-'`"
 ## end vars
 #############
 
 
 
 if [[ ${DEBUG} = "TRUE" ]] ; then
-	echo || echo
-	echo "DEBUG = ${DEBUG}"
-	echo && echo
-	echo "MOODLE PATH: 				${MOODLEPATH}"
-	echo "BACKUP PATH: 				${BACKUPPATH}"
-	echo "Site Name: 					${_sitename}"
-	echo "Timestamp: 					${_now}"
-	echo "Moodle files archive file: 	${_files_zipfile}"
-	echo "Moodle data archive file: 	${_data_zipfile}"
-	echo || echo
-	echo "mdl_conf dump:"
-	cat ~/mdl_conf.sh
-	echo || echo
-	echo "Note: backup has not occurred, omit debug parameter to execute backup"
-	echo || echo
-else	
-	###################
-	# start the backup
-	# backup the moodle files
-	echo -e "Starting backup of ${MOODLEPATH} directory of site ${_sitename} timestamped at datetime $_now"
-	tar -vczf ${_files_zipfile} ${MOODLEPATH}
+        echo || echo
+        echo "DEBUG = ${DEBUG}"
+        echo && echo
+        echo "MOODLE PATH:                              ${MOODLEPATH}"
+        echo "BACKUP PATH:                              ${BACKUPPATH}"
+        echo "Site Name:                                        ${_sitename}"
+        echo "Timestamp:                                        ${_now}"
+        echo "Moodle files archive file:        ${_files_zipfile}"
+        echo "Moodle data archive file:         ${_data_zipfile}"
+        echo || echo
+        echo "mdl_conf dump:"
+        cat ~/mdl_conf.sh
+        echo || echo
+        echo -e "${warn} Note: backup has not occurred, omit debug parameter to execute backup"
+        echo || echo
+else
+        ###################
+        # start the backup
+        # backup the moodle files
+        echo -e "${info} Starting backup of ${MOODLEPATH} directory of site ${_sitename} timestamped at datetime $_now"
+        #tar -vczf ${_files_zipfile} ${MOODLEPATH}
+        tar zcf - ${MOODLEPATH} -P | pv -s $(du -sb ${MOODLEPATH} | awk '{print $1}') | gzip > ${_files_zipfile}
+        checkerr "$_" "$?"
 
-	echo || echo
+        echo || echo
 
-	# backup moodledata
-	echo -e "Starting backup of ${dataroot} Moodle data directory of site ${_sitename} timestamped at datetime $_now"
-	tar -vczf ${_data_zipfile} ${dataroot}
+        # backup moodledata
+        echo -e "${info} Starting backup of ${dataroot} Moodle data directory of site ${_sitename} timestamped at datetime $_now"
+        #tar -vczf ${_data_zipfile} ${dataroot}
+        tar zcf - ${dataroot} -P | pv -s $(du -sb ${dataroot} | awk '{print $1}') | gzip > ${_data_zipfile}
+        checkerr "$_" "$?"
 
-	echo || echo
+        echo || echo
 
-	# end of backup
-	echo -e "Ending backup of site ${_sitename} timestamped at datetime $_now"
-	echo -e "_________________________________________________"
+        # end of backup
+        echo -e "${info} Ending backup of site ${_sitename} timestamped at datetime $_now"
+        echo -e "_________________________________________________"
 fi
 
 ### Get rid of mdl_conf when finished
 if [ -e ~/mdl_conf.sh ]; then
-        rm -f ~/mdl_conf.sh 
-			if [ $? -eq 0 ]; then # if OK
-			  echo "mdl_conf removed [OK]"
-			else
-			   err_exit
-			fi
+        rm -f ~/mdl_conf.sh
+                        if [ $? -eq 0 ]; then # if OK
+                          echo "mdl_conf removed [OK]"
+                        else
+                           err_exit
+                        fi
 fi
 
 ## Clean up old backups
